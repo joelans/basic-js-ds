9eb2bebb8181c0c7a203ad0e0cd1cf34c422a1ca
joelans <baileyderek59@gmail.com>
2025-01-06 23:32:43 +0300

    feat: implement stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  arr = [];
+  index = 0;
+
+  push(element) {
+    this.arr.push(element);
+    this.index += 1;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.index -= 1;
+    return this.arr.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr[this.index - 1];
   }
 }
 
